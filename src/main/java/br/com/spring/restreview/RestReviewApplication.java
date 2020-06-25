a4a6dc832391b6a2a5df6ad8b510d330584dadc0
Allan Torres <allan.torres@arce.ce.gov.br>
2020-06-24 21:31:52 -0300

    first commit

---
+++ src/main/java/br/com/spring/restreview/RestReviewApplication.java
@@ -0,0 +1,13 @@
+package br.com.spring.restreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/spring/restreview/RestReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spring.restreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
