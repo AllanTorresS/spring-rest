c57fa41ac30ace176d4e20389422db54005d6d1e
Allan Torres <allan.torres@arce.ce.gov.br>
2020-07-19 18:42:51 -0300

    adicionando e configurando flyway

--- src/main/java/br/com/spring/restreview/model/Restaurante.java
+++ src/main/java/br/com/spring/restreview/model/Restaurante.java
@@ -1,6 +1,8 @@
 package br.com.spring.restreview.model;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.CreationTimestamp;
@@ -15,10 +17,18 @@ import java.util.List;
 @Table(name = "restaurante")
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+/**
+ * @JsonIdentityInfo é usado para resolver o problema de depencia circular,
+ * nesse caso a classe restaurante tem uma cozinha que tem uma lista de restaurantes,
+ * com isso ocorreria um stackoverflow, mas com essa anotação na clase que vai ser pesquisada
+ * isso não ocorre
+ */
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "id")
 public class Restaurante {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "restaurante_seq_id")
-    @SequenceGenerator(name = "reataurante_seq_id",sequenceName = "restaurante_seq_id",allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "restaurante_seq_id")
+    @SequenceGenerator(name = "restaurante_seq_id", sequenceName = "restaurante_seq_id", allocationSize = 1)
     @EqualsAndHashCode.Include
     private Long id;
 
@@ -28,17 +38,15 @@ public class Restaurante {
     private BigDecimal taxaFrete;
 
     @CreationTimestamp
-
     private LocalDateTime dataCadastro;
 
     @UpdateTimestamp
-
     private LocalDateTime dataAtualizacao;
 
     @Embedded
     private Endereco endereco;
 
-    @JsonIgnore
+
     @ManyToOne
     @JoinColumn(name = "cozinha_id")
     private Cozinha cozinha;
---
+++ src/main/resources/db/migration/V001__CRIAR_SEQUENCES.sql
@@ -0,0 +1,10 @@
+create sequence cidade_seq_id start 1 increment 1;
+create sequence cozinha_seq_id start 1 increment 1;
+create sequence estado_seq_id start 1 increment 1;
+create sequence forma_pagamento_seq_id start 1 increment 1;
+create sequence grupo_seq_id start 1 increment 1;
+create sequence permissao_seq_id start 1 increment 1;
+create sequence produto_seq_id start 1 increment 1;
+create sequence restaurante_seq_id start 1 increment 1;
+create sequence usuario_seq_id start 1 increment 1;
+
